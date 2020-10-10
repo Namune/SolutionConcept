ad2bdca82ce48ad97baec4030b38111a41903af5
Kenji Surya Utama <kenjisuryautama@gmail.com>
2020-10-10 16:40:12 +0700

    done TabBar (home, ticket, notif, profile)

---
+++ SolutionConcept/View/TicketView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketView.swift
+//  SolutionConcept
+//
+//  Created by Kenji Surya Utama on 10/10/20.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text("Ini Tiket")
+    }
+}
+
+struct TicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketView()
+    }
+}
